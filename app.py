f35c733bcfa4fe117a45a307910b862407b58eab
mfcsds <fathur.cs.ds@gmail.com>
2024-10-30 14:23:06 +0700

    fix:added some ui components

--- app.py
+++ app.py
@@ -2,3 +2,28 @@ import streamlit as st
 
 st.write("Halllo World")
 st.title("Demo Aplikasi Ashoka / Viume")
+
+import streamlit as st
+
+st.button("Reset", type="primary")
+
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+import streamlit as st
+
+color = st.select_slider(
+    "Select a color of the rainbow",
+    options=[
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+    ],
+)
+st.write("My favorite color is", color)
